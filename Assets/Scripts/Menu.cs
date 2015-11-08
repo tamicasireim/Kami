3c167526c0f59fe642063f65b61dc0ff7ff4f707
Theo Judas <theo_jd@hotmail.fr>
2015-11-08 19:18:37 +0100

    Ajout du menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public void Quit()
+	{
+		// Ferme l'application
+		Application.Quit();
+	}
+	
+	public void LoadScene()
+	{
+		// Charge la scène principale
+		Application.LoadLevel("scene");
+	}
+}
